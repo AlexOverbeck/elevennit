c323e4392a25d22ce70d38dd7de38e5ed123ab51
Alex Overbeck <alexoverbeck@gmail.com>
2014-11-10 19:15:52 -0500

    hook up posts#create action in the controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,18 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    post = Post.new allowed_params
+    if post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def allowed_params
+    params.require(:post).permit(:title, :link, :body)
+  end
 end
