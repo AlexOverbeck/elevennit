84d7bf99a1faed70830acae85b0902b29a9ca41d
Alex Overbeck <alexoverbeck@gmail.com>
2014-11-11 12:51:39 -0500

    add validation for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
-  validates_presence_of :title, :link
-  validates :title, length: { maximum: 255 }
+  validates :title, length: { maximum: 255 }, presence: true
+  validates :link, length: { maximum: 2000 }, presence: true, if: :link?
+  validates :body, presence: true, if: :test?
   enum post_type: [:link, :text]
 
 end
